f125ffb111c0520c71deaea4db16a9242ab6f27d
John Wästerlund <grunka@gmail.com>
2017-12-02 10:24:36 +0100

    Moving files

--- src/main/java/com/grunka/adventofcode/DayOne.java
+++ src/main/java/com/grunka/adventofcode/DayOne.java
@@ -1,4 +1,4 @@
-package com.grunka.adventofcode.one;
+package com.grunka.adventofcode;
 
 import java.util.ArrayList;
 import java.util.Arrays;
---
+++ src/main/java/com/grunka/adventofcode/DayTwo.java
@@ -0,0 +1,4 @@
+package com.grunka.adventofcode;
+
+public class DayTwo {
+}
