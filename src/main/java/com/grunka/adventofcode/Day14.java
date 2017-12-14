5307a921dfd16258b5a05a6ed922428309f76a67
John Wästerlund <grunka@gmail.com>
2017-12-14 11:42:34 +0100

    Making part one take a consumer to be able to collect the whole grid in part two

--- src/main/java/com/grunka/adventofcode/Day14.java
+++ src/main/java/com/grunka/adventofcode/Day14.java
@@ -1,30 +1,65 @@
 package com.grunka.adventofcode;
 
 import java.util.Arrays;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.function.Consumer;
 import java.util.stream.Collectors;
 
 public class Day14 {
     public static void main(String[] args) {
         String input = "ugkiagan";
-        int total = 0;
+        part1(input);
+        part2(input);
+    }
+
+    private static void part2(String input) {
+        System.out.println("Part 2");
+        int[][] grid = new int[128][];
+        AtomicInteger row = new AtomicInteger();
+        Consumer<String> lineEater = line -> grid[row.getAndIncrement()] = Arrays.stream(line.split("")).mapToInt(Integer::parseInt).toArray();
+        generateGrid(input, lineEater);
+
+        int groups = 0;
+        for (int x = 0; x < 128; x++) {
+            for (int y = 0; y < 128; y++) {
+                if (grid[x][y] == 1) {
+                    groups++;
+                    floodFill(grid, x, y);
+                }
+            }
+        }
+        System.out.println("groups = " + groups);
+        System.out.println();
+    }
+
+    private static void floodFill(int[][] grid, int x, int y) {
+        if (x < 0 || y < 0 || x > 127 || y > 127 || grid[x][y] == 0) {
+            return;
+        }
+        grid[x][y] = 0;
+        floodFill(grid, x - 1, y);
+        floodFill(grid, x + 1, y);
+        floodFill(grid, x, y - 1);
+        floodFill(grid, x, y + 1);
+    }
+
+    private static void part1(String input) {
+        System.out.println("Part 1");
+        AtomicInteger total = new AtomicInteger();
+        Consumer<String> lineEater = line -> total.addAndGet(Arrays.stream(line.split("")).mapToInt(Integer::parseInt).sum());
+        generateGrid(input, lineEater);
+        System.out.println("total = " + total);
+        System.out.println();
+    }
+
+    private static void generateGrid(String input, Consumer<String> lineEater) {
         for (int i = 0; i < 128; i++) {
             String hash = Day10.knotHash(input + "-" + i);
             String line = Arrays.stream(hash.split(""))
                     .map(hex -> Integer.parseInt(hex, 16))
                     .map(n -> String.format("%4s", Integer.toBinaryString(n)).replace(' ', '0'))
                     .collect(Collectors.joining());
-            int used = Arrays.stream(line.split("")).reduce("", (a, b) -> {
-                if ("0".equals(a)) {
-                    a = "";
-                }
-                if ("0".equals(b)) {
-                    b = "";
-                }
-                return a + b;
-            }).length();
-            total += used;
-            System.out.println("line = " + line);
+            lineEater.accept(line);
         }
-        System.out.println("total = " + total);
     }
 }
