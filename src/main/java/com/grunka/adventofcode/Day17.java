f86aae3c1796536ef4fe2e4956c56af302bca224
John Wästerlund <grunka@gmail.com>
2017-12-17 19:34:52 +0100

    Faster but still not fast enough

--- src/main/java/com/grunka/adventofcode/Day17.java
+++ src/main/java/com/grunka/adventofcode/Day17.java
@@ -1,8 +1,5 @@
 package com.grunka.adventofcode;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class Day17 {
     public static void main(String[] args) {
         part1();
@@ -11,32 +8,66 @@ public class Day17 {
 
     private static void part2() {
         System.out.println("Part 2");
-        List<Integer> buffer = doInserts(50_000_000);
-        int position = buffer.indexOf(0);
-        System.out.println("position = " + position);
-        System.out.println("buffer.get(position + 1) = " + buffer.get(position + 1));
+        Buffer buffer = doInserts(50_000_000);
+        Buffer zero = buffer.find(0);
+        System.out.println("zero.next.value = " + zero.next.value);
     }
 
     private static void part1() {
         System.out.println("Part 1");
-        List<Integer> buffer = doInserts(2017);
-        int position = buffer.indexOf(2017);
-        System.out.println("position = " + position);
-        System.out.println("buffer.get(position + 1) = " + buffer.get(position + 1));
+        Buffer buffer = doInserts(2017);
+        System.out.println("buffer.next.value = " + buffer.next.value);
     }
 
-    private static List<Integer> doInserts(int n) {
-        int position = 0;
+    private static Buffer doInserts(int n) {
         int steps = 345;
-        List<Integer> buffer = new ArrayList<>();
+        Buffer buffer = new Buffer(0);
         for (int i = 0; i < n; i++) {
-            position = (position + steps) % (i + 1);
-            buffer.add(position, i + 1);
-            position++;
-            if (i % 10_000 == 0) {
+            buffer = buffer.step(steps);
+            buffer = buffer.insert(i + 1);
+            if (i % 100_000 == 0) {
                 System.out.println("i = " + i);
             }
         }
         return buffer;
+    }
+
+    private static class Buffer {
+        static int size;
+        Buffer next;
+        final int value;
+
+        Buffer(int value) {
+            this.value = value;
+            this.next = this;
+            size = 1;
+        }
+
+        Buffer(int value, Buffer next) {
+            this.value = value;
+            this.next = next;
+        }
+
+        Buffer insert(int value) {
+            next = new Buffer(value, next);
+            size++;
+            return next;
+        }
+
+        Buffer find(int value) {
+            Buffer current = this;
+            while (current.value != value) {
+                current = current.next;
+            }
+            return current;
+        }
+
+        Buffer step(int steps) {
+            Buffer current = this;
+            for (int i = 0; i < steps; i++) {
+                current = current.next;
+            }
+            return current;
+        }
     }
 }
