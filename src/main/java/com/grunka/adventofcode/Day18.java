1e8c2f660363b2780bad24930b3671f32ea9e183
John Wästerlund <grunka@gmail.com>
2017-12-18 15:16:52 +0100

    Changes in preparation for part 2

--- src/main/java/com/grunka/adventofcode/Day18.java
+++ src/main/java/com/grunka/adventofcode/Day18.java
@@ -11,71 +11,88 @@ import java.util.stream.Collectors;
 public class Day18 {
     public static void main(String[] args) {
         AtomicLong playingSound = new AtomicLong();
-        AtomicLong programPointer = new AtomicLong();
-        HashMap<String, Long> registers = new HashMap<>();
-        Consumer<Long> recover = i -> {
-            System.out.println("i = " + i);
+        Program.create(INPUT, 0, playingSound::set, () -> {
+            System.out.println(playingSound.get());
             System.exit(0);
-        };
-        List<Runnable> instructions = Arrays.stream(INPUT.split("\n")).map(row -> {
-            String[] arguments = row.substring(4).split(" ");
-            Supplier<Long> getX;
-            Consumer<Long> setX;
-            if (isRegister(arguments[0])) {
-                getX = () -> registers.computeIfAbsent(arguments[0], k -> 0L);
-                setX = i -> registers.put(arguments[0], i);
-            } else {
-                long n = Long.parseLong(arguments[0]);
-                getX = () -> n;
-                setX = i -> {
-                    throw new UnsupportedOperationException("Tried to set X value to constant on row " + row);
-                };
-            }
-            Supplier<Long> getY;
-            if (arguments.length != 2) {
-                getY = () -> {
-                    throw new UnsupportedOperationException("Tried to get Y value on row without Y value " + row);
-                };
-            } else {
-                if (isRegister(arguments[1])) {
-                    getY = () -> registers.computeIfAbsent(arguments[1], k -> 0L);
+            return 0L;
+        }).run();
+    }
+
+    private static class Program implements Runnable {
+        private final List<Runnable> instructions;
+        private final AtomicLong programPointer;
+        private final HashMap<String, Long> registers;
+
+        private Program(List<Runnable> instructions, AtomicLong programPointer, HashMap<String, Long> registers) {
+            this.instructions = instructions;
+            this.programPointer = programPointer;
+            this.registers = registers;
+        }
+
+        public static Program create(String input, long id, Consumer<Long> send, Supplier<Long> receive) {
+            AtomicLong programPointer = new AtomicLong();
+            HashMap<String, Long> registers = new HashMap<>();
+            registers.put("p", id);
+            List<Runnable> instructions = Arrays.stream(input.split("\n")).map(row -> {
+                String[] arguments = row.substring(4).split(" ");
+                Supplier<Long> getX;
+                Consumer<Long> setX;
+                if (isRegister(arguments[0])) {
+                    getX = () -> registers.computeIfAbsent(arguments[0], k -> 0L);
+                    setX = i -> registers.put(arguments[0], i);
                 } else {
-                    long n = Long.parseLong(arguments[1]);
-                    getY = () -> n;
+                    long n = Long.parseLong(arguments[0]);
+                    getX = () -> n;
+                    setX = i -> {
+                        throw new UnsupportedOperationException("Tried to set X value to constant on row " + row);
+                    };
                 }
-            }
-
-            switch (row.substring(0, 3)) {
-                case "snd":
-                    return (Runnable) () -> playingSound.set(getX.get());
-                case "set":
-                    return (Runnable) () -> setX.accept(getY.get());
-                case "add":
-                    return (Runnable) () -> setX.accept(getX.get() + getY.get());
-                case "mul":
-                    return (Runnable) () -> setX.accept(getX.get() * getY.get());
-                case "mod":
-                    return (Runnable) () -> setX.accept(getX.get() % getY.get());
-                case "rcv":
-                    return (Runnable) () -> {
-                        if (getX.get() != 0) {
-                            recover.accept(playingSound.get());
-                        }
+                Supplier<Long> getY;
+                if (arguments.length != 2) {
+                    getY = () -> {
+                        throw new UnsupportedOperationException("Tried to get Y value on row without Y value " + row);
                     };
-                case "jgz":
-                    return (Runnable) () -> {
-                        if (getX.get() != 0) {
-                            programPointer.addAndGet(getY.get() - 1);
-                        }
-                    };
-                default:
-                    throw new UnsupportedOperationException("Did not recognize operator on row " + row);
-            }
-        }).collect(Collectors.toList());
+                } else {
+                    if (isRegister(arguments[1])) {
+                        getY = () -> registers.computeIfAbsent(arguments[1], k -> 0L);
+                    } else {
+                        long n = Long.parseLong(arguments[1]);
+                        getY = () -> n;
+                    }
+                }
 
-        while (programPointer.get() < instructions.size()) {
-            instructions.get((int) programPointer.getAndIncrement()).run();
-            System.out.println("registers = " + registers);
+                switch (row.substring(0, 3)) {
+                    case "snd":
+                        return (Runnable) () -> send.accept(getX.get());
+                    case "set":
+                        return (Runnable) () -> setX.accept(getY.get());
+                    case "add":
+                        return (Runnable) () -> setX.accept(getX.get() + getY.get());
+                    case "mul":
+                        return (Runnable) () -> setX.accept(getX.get() * getY.get());
+                    case "mod":
+                        return (Runnable) () -> setX.accept(getX.get() % getY.get());
+                    case "rcv":
+                        return (Runnable) () -> setX.accept(receive.get());
+                    case "jgz":
+                        return (Runnable) () -> {
+                            if (getX.get() != 0) {
+                                programPointer.addAndGet(getY.get() - 1);
+                            }
+                        };
+                    default:
+                        throw new UnsupportedOperationException("Did not recognize operator on row " + row);
+                }
+            }).collect(Collectors.toList());
+            return new Program(instructions, programPointer, registers);
+        }
+
+        @Override
+        public void run() {
+            while (programPointer.get() < instructions.size()) {
+                instructions.get((int) programPointer.getAndIncrement()).run();
+                //System.out.println("registers = " + registers);
+            }
         }
     }
 
