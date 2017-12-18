4165090fbb0a1941aca52c7d841169f976d17cf1
John Wästerlund <grunka@gmail.com>
2017-12-18 17:20:37 +0100

    Figured out a mistake with the jgz instruction

--- src/main/java/com/grunka/adventofcode/Day18.java
+++ src/main/java/com/grunka/adventofcode/Day18.java
@@ -3,33 +3,80 @@ package com.grunka.adventofcode;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicInteger;
 import java.util.concurrent.atomic.AtomicLong;
 import java.util.function.Consumer;
 import java.util.function.Supplier;
 import java.util.stream.Collectors;
 
 public class Day18 {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
+        part1();
+        part2();
+    }
+
+    private static void part2() throws InterruptedException {
+        System.out.println("Part 2");
+        BlockingQueue<Long> to0 = new LinkedBlockingQueue<>();
+        BlockingQueue<Long> to1 = new LinkedBlockingQueue<>();
+        AtomicInteger sendCounter = new AtomicInteger();
+        Program program0 = Program.create(INPUT, 0, to1::offer, receiver(to0));
+        Program program1 = Program.create(INPUT, 1, l -> {
+            to0.offer(l);
+            sendCounter.incrementAndGet();
+        }, receiver(to1));
+        Thread thread0 = new Thread(program0);
+        Thread thread1 = new Thread(program1);
+        thread0.start();
+        thread1.start();
+        thread0.join();
+        thread1.join();
+        System.out.println("sendCounter = " + sendCounter);
+    }
+
+    private static Supplier<Long> receiver(BlockingQueue<Long> to1) {
+        return () -> {
+            try {
+                Long value = to1.poll(1, TimeUnit.SECONDS);
+                if (value == null) {
+                    throw new ProgramEnded();
+                }
+                return value;
+            } catch (InterruptedException e) {
+                throw new Error();
+            }
+        };
+    }
+
+    private static void part1() {
+        System.out.println("Part 1");
         AtomicLong playingSound = new AtomicLong();
         Program.create(INPUT, 0, playingSound::set, () -> {
             System.out.println(playingSound.get());
-            System.exit(0);
-            return 0L;
+            throw new ProgramEnded();
         }).run();
     }
 
+    private static class ProgramEnded extends RuntimeException {
+    }
+
     private static class Program implements Runnable {
+        private final long id;
         private final List<Runnable> instructions;
         private final AtomicLong programPointer;
         private final HashMap<String, Long> registers;
 
-        private Program(List<Runnable> instructions, AtomicLong programPointer, HashMap<String, Long> registers) {
+        private Program(long id, List<Runnable> instructions, AtomicLong programPointer, HashMap<String, Long> registers) {
+            this.id = id;
             this.instructions = instructions;
             this.programPointer = programPointer;
             this.registers = registers;
         }
 
-        public static Program create(String input, long id, Consumer<Long> send, Supplier<Long> receive) {
+        static Program create(String input, long id, Consumer<Long> send, Supplier<Long> receive) {
             AtomicLong programPointer = new AtomicLong();
             HashMap<String, Long> registers = new HashMap<>();
             registers.put("p", id);
@@ -76,7 +123,7 @@ public class Day18 {
                         return (Runnable) () -> setX.accept(receive.get());
                     case "jgz":
                         return (Runnable) () -> {
-                            if (getX.get() != 0) {
+                            if (getX.get() > 0) {
                                 programPointer.addAndGet(getY.get() - 1);
                             }
                         };
@@ -84,14 +131,18 @@ public class Day18 {
                         throw new UnsupportedOperationException("Did not recognize operator on row " + row);
                 }
             }).collect(Collectors.toList());
-            return new Program(instructions, programPointer, registers);
+            return new Program(id, instructions, programPointer, registers);
         }
 
         @Override
         public void run() {
-            while (programPointer.get() < instructions.size()) {
-                instructions.get((int) programPointer.getAndIncrement()).run();
-                //System.out.println("registers = " + registers);
+            try {
+                while (programPointer.get() < instructions.size()) {
+                    instructions.get((int) programPointer.getAndIncrement()).run();
+                    System.out.println(id + ": registers = " + registers);
+                }
+            } catch (ProgramEnded e) {
+                System.out.println("Program ended");
             }
         }
     }
@@ -99,6 +150,14 @@ public class Day18 {
     private static boolean isRegister(String s) {
         return s.length() == 1 && s.charAt(0) >= 'a' && s.charAt(0) <= 'z';
     }
+
+    private static final String TEST_INPUT_2 = "snd 1\n" +
+            "snd 2\n" +
+            "snd p\n" +
+            "rcv a\n" +
+            "rcv b\n" +
+            "rcv c\n" +
+            "rcv d";
 
     private static final String TEST_INPUT = "set a 1\n" +
             "add a 2\n" +
