9aa6427d3ef15335f47f9615e78d0857dfc7a8cb
John Wästerlund <grunka@gmail.com>
2017-12-21 11:17:20 +0100

    Something is still wrong, probably with the flipping/rotation

--- src/main/java/com/grunka/adventofcode/Day21.java
+++ src/main/java/com/grunka/adventofcode/Day21.java
@@ -1,8 +1,6 @@
 package com.grunka.adventofcode;
 
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 import java.util.stream.Collectors;
 
 public class Day21 {
@@ -10,20 +8,38 @@ public class Day21 {
         Map<String, String> rules = new HashMap<>();
         Arrays.stream(INPUT.split("\n")).forEach(rule -> {
             String[] inOut = rule.split(" => ");
-            rules.put(inOut[0], inOut[1]);
-            rules.put(flipVertical(inOut[0]), inOut[1]);
-            rules.put(flipHorizontal(inOut[0]), inOut[1]);
-            rules.put(rotateRight(inOut[0]), inOut[1]);
-            rules.put(rotateLeft(inOut[0]), inOut[1]);
+            for (String block : permutations(inOut[0])) {
+                rules.put(block, inOut[1]);
+            }
         });
         String[] blocks = imageToBlocks(IMAGE);
         System.out.println("blocks = " + Arrays.toString(blocks));
+        System.out.println("permutations(blocks[0]) = " + permutations(blocks[0]));
         String image = blocksToImage(blocks);
         System.out.println("image = " + image);
         System.out.println("rules = " + rules);
         String[] newBlocks = process(blocks, rules);
         String newImage = blocksToImage(newBlocks);
         System.out.println("newImage = " + newImage);
+    }
+
+    private static Collection<String> permutations(String block) {
+        Set<String> result = new HashSet<>();
+        result.add(block);
+        for (int i = 0; i < 100; i++) {
+            Set<String> additions = new HashSet<>();
+            for (String s : result) {
+                additions.add(flipHorizontal(s));
+                additions.add(flipVertical(s));
+                additions.add(flipHorizontal(flipVertical(s)));
+                additions.add(rotateLeft(s));
+                additions.add(rotateLeft(rotateLeft(s)));
+                additions.add(rotateLeft(rotateLeft(rotateLeft(s))));
+            }
+            result.addAll(additions);
+        }
+        System.out.println("result.size() = " + result.size());
+        return result;
     }
 
     private static String[] process(String[] input, Map<String, String> rules) {
@@ -79,18 +95,23 @@ public class Day21 {
     }
 
     private static String rotateLeft(String input) {
-        return null;
-    }
+        if (input.length() == 5) {
+            return "" + input.charAt(1) + input.charAt(4) + "/" + input.charAt(0) + input.charAt(3);
+        } else if (input.length() == 11) {
+            return "" + input.charAt(2) + input.charAt(6) + input.charAt(0) + "/" +
+                    input.charAt(1) + input.charAt(5) + input.charAt(9) + "/" +
+                    input.charAt(0) + input.charAt(4) + input.charAt(10);
 
-    private static String rotateRight(String input) {
-        return null;
+        } else {
+            throw new IllegalArgumentException("Unflippable " + input);
+        }
     }
 
     private static String flipHorizontal(String input) {
         if (input.length() == 5) {
-            return input.charAt(1) + input.charAt(0) + "/" + input.charAt(4) + input.charAt(3);
+            return "" + input.charAt(1) + input.charAt(0) + "/" + input.charAt(4) + input.charAt(3);
         } else if (input.length() == 11) {
-            return input.charAt(2) + input.charAt(1) + input.charAt(0) + "/" +
+            return "" + input.charAt(2) + input.charAt(1) + input.charAt(0) + "/" +
                     input.charAt(6) + input.charAt(5) + input.charAt(4) + "/" +
                     input.charAt(10) + input.charAt(9) + input.charAt(8);
 
