6f8ca392faa678dc5041e3cd5ac8f3a155f9e957
John Wästerlund <grunka@gmail.com>
2017-12-26 15:26:05 +0100

    Equals and hash code to be able to create a lookup table instead of the long string

--- src/main/java/com/grunka/adventofcode/Day22.java
+++ src/main/java/com/grunka/adventofcode/Day22.java
@@ -2,6 +2,7 @@ package com.grunka.adventofcode;
 
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.Objects;
 import java.util.stream.Collectors;
 
 import static com.grunka.adventofcode.Day22.Direction.*;
@@ -110,6 +111,20 @@ public class Day22 {
                 default:
                     throw new IllegalStateException();
             }
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            Position position = (Position) o;
+            return x == position.x &&
+                    y == position.y;
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(x, y);
         }
     }
 
