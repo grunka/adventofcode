a404088cfc12d63a188a7c3a748c6d054d9973ea
John Wästerlund <grunka@gmail.com>
2017-12-27 01:28:01 +0100

    Starting out on day 24

---
+++ src/main/java/com/grunka/adventofcode/Day24.java
@@ -0,0 +1,113 @@
+package com.grunka.adventofcode;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Day24 {
+    public static void main(String[] args) {
+        List<Port> ports = Arrays.stream(INPUT.split("\n")).map(row -> {
+            String[] port = row.split("/");
+            return new Port(Integer.parseInt(port[0]), Integer.parseInt(port[1]));
+        }).collect(Collectors.toList());
+        List<Port> starts = ports.stream().filter(p -> p.has(0)).collect(Collectors.toList());
+        System.out.println("starts = " + starts);
+        for (Port start : starts) {
+
+        }
+    }
+
+    private static class Port {
+        final int a;
+        final int b;
+
+        private Port(int a, int b) {
+            this.a = a;
+            this.b = b;
+        }
+
+        boolean has(int i) {
+            return a == i || b == i;
+        }
+
+        public int strength() {
+            return a + b;
+        }
+
+        public int other(int i) {
+            if (i == a) {
+                return b;
+            }
+            if (i == b) {
+                return a;
+            }
+            throw new IllegalArgumentException("Did not match any port");
+        }
+
+        @Override
+        public String toString() {
+            return "Port{" +
+                    "a=" + a +
+                    ", b=" + b +
+                    '}';
+        }
+    }
+
+    private static final String INPUT = "42/37\n" +
+            "28/28\n" +
+            "29/25\n" +
+            "45/8\n" +
+            "35/23\n" +
+            "49/20\n" +
+            "44/4\n" +
+            "15/33\n" +
+            "14/19\n" +
+            "31/44\n" +
+            "39/14\n" +
+            "25/17\n" +
+            "34/34\n" +
+            "38/42\n" +
+            "8/42\n" +
+            "15/28\n" +
+            "0/7\n" +
+            "49/12\n" +
+            "18/36\n" +
+            "45/45\n" +
+            "28/7\n" +
+            "30/43\n" +
+            "23/41\n" +
+            "0/35\n" +
+            "18/9\n" +
+            "3/31\n" +
+            "20/31\n" +
+            "10/40\n" +
+            "0/22\n" +
+            "1/23\n" +
+            "20/47\n" +
+            "38/36\n" +
+            "15/8\n" +
+            "34/32\n" +
+            "30/30\n" +
+            "30/44\n" +
+            "19/28\n" +
+            "46/15\n" +
+            "34/50\n" +
+            "40/20\n" +
+            "27/39\n" +
+            "3/14\n" +
+            "43/45\n" +
+            "50/42\n" +
+            "1/33\n" +
+            "6/39\n" +
+            "46/44\n" +
+            "22/35\n" +
+            "15/20\n" +
+            "43/31\n" +
+            "23/23\n" +
+            "19/27\n" +
+            "47/15\n" +
+            "43/43\n" +
+            "25/36\n" +
+            "26/38\n" +
+            "1/10";
+}
