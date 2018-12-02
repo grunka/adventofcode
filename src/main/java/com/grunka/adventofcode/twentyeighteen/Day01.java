069ac1e0347e004b17febc5808b9a6ad9ebd46b0
John Wästerlund <grunka@gmail.com>
2018-12-02 10:34:15 +0100

    Part 1 for day 1

--- src/main/java/com/grunka/adventofcode/twentyeighteen/Day01.java
+++ src/main/java/com/grunka/adventofcode/twentyeighteen/Day01.java
@@ -1,7 +1,24 @@
 package com.grunka.adventofcode.twentyeighteen;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Day01 {
-    public static void main(String[] args) {
-        System.out.println("Waiting!");
+    public static void main(String[] args) throws IOException {
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(Day01.class.getResourceAsStream("/twentyeighteen/Day01-1.txt")))) {
+            String line;
+            int result = 0;
+            while ((line = reader.readLine()) != null) {
+                boolean add = line.charAt(0) == '+';
+                int value = Integer.parseInt(line.substring(1));
+                if (add) {
+                    result += value;
+                } else {
+                    result -= value;
+                }
+            }
+            System.out.println("result = " + result);
+        }
     }
 }
