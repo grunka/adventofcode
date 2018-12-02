c718ca0bbdfbf6a3a1b9735f186593ae6e4bd794
John Wästerlund <grunka@gmail.com>
2018-12-02 10:46:04 +0100

    Part 2 for day 1, and cleanup of part 1

--- src/main/java/com/grunka/adventofcode/twentyeighteen/Day01.java
+++ src/main/java/com/grunka/adventofcode/twentyeighteen/Day01.java
@@ -3,22 +3,51 @@ package com.grunka.adventofcode.twentyeighteen;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 public class Day01 {
     public static void main(String[] args) throws IOException {
+        part1();
+        part2();
+    }
+
+    private static void part1() throws IOException {
+        int sum = getValues().stream().mapToInt(i -> i).sum();
+        System.out.println("Part 1 result: " + sum);
+    }
+
+    private static void part2() throws IOException {
+        List<Integer> values = getValues();
+        Set<Integer> seen = new HashSet<>();
+        int result = 0;
+        int i = 0;
+        boolean found = false;
+        while (!found) {
+            int value = values.get(i);
+            i = (i + 1) % values.size();
+            result += value;
+            found = !seen.add(result);
+        }
+        System.out.println("Part 2 result: " + result);
+    }
+
+    private static List<Integer> getValues() throws IOException {
+        List<Integer> values = new ArrayList<>();
         try (BufferedReader reader = new BufferedReader(new InputStreamReader(Day01.class.getResourceAsStream("/twentyeighteen/Day01-1.txt")))) {
             String line;
-            int result = 0;
             while ((line = reader.readLine()) != null) {
                 boolean add = line.charAt(0) == '+';
                 int value = Integer.parseInt(line.substring(1));
                 if (add) {
-                    result += value;
+                    values.add(value);
                 } else {
-                    result -= value;
+                    values.add(-value);
                 }
             }
-            System.out.println("result = " + result);
         }
+        return values;
     }
 }
