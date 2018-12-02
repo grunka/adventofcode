7bd6d5b456f5217db7f9d6baf714d8350bcc8ab7
John Wästerlund <grunka@gmail.com>
2018-12-02 11:29:31 +0100

    Part 2 done

--- src/main/java/com/grunka/adventofcode/twentyeighteen/Day02.java
+++ src/main/java/com/grunka/adventofcode/twentyeighteen/Day02.java
@@ -11,6 +11,34 @@ import java.util.stream.Collectors;
 
 public class Day02 {
     public static void main(String[] args) throws URISyntaxException, IOException {
+        part1();
+        part2();
+    }
+
+    private static void part2() throws URISyntaxException, IOException {
+        List<String> lines = Files.readAllLines(Paths.get(Day02.class.getResource("/twentyeighteen/Day02-1.txt").toURI()));
+        while (lines.size() > 1) {
+            String candidate = lines.get(0);
+            lines = lines.subList(1, lines.size());
+            lines.forEach(line -> {
+                int differences = 0;
+                char lastDifferent = 0;
+                for (int i = 0; i < candidate.length(); i++) {
+                    if (line.charAt(i) != candidate.charAt(i)) {
+                        differences++;
+                        lastDifferent = candidate.charAt(i);
+                    }
+                }
+                if (differences == 1) {
+                    char different = lastDifferent;
+                    String common = Arrays.stream(candidate.split("")).filter(s -> s.charAt(0) != different).collect(Collectors.joining());
+                    System.out.println("Part 2 result: " + common);
+                }
+            });
+        }
+    }
+
+    private static void part1() throws IOException, URISyntaxException {
         List<String> lines = Files.readAllLines(Paths.get(Day02.class.getResource("/twentyeighteen/Day02-1.txt").toURI()));
         int twos = lines.stream().filter(s -> hasExactCount(2, s)).mapToInt(s -> 1).sum();
         int threes = lines.stream().filter(s -> hasExactCount(3, s)).mapToInt(s -> 1).sum();
