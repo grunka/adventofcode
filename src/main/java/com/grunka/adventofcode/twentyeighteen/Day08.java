75e978dba88e833f1c814a9865f102a0b74563f7
John Wästerlund <grunka@gmail.com>
2018-12-11 19:56:03 +0100

    Part 1

--- src/main/java/com/grunka/adventofcode/twentyeighteen/Day08.java
+++ src/main/java/com/grunka/adventofcode/twentyeighteen/Day08.java
@@ -5,23 +5,25 @@ import java.net.URISyntaxException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.Arrays;
-import java.util.Stack;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.function.Consumer;
 
 public class Day08 {
     public static void main(String[] args) throws URISyntaxException, IOException {
         int[] input = Arrays.stream(Files.readString(Paths.get(Day08.class.getResource("/twentyeighteen/Day08-1.txt").toURI())).trim().split(" ")).mapToInt(Integer::parseInt).toArray();
-        int position = 0;
-        Stack<Tree> treeStack = new Stack<>();
-        treeStack.push(new Tree(new Tree[input[position]], new int[input[position + 1]]));
+        AtomicInteger sum = new AtomicInteger();
+        consume(input, metadata -> Arrays.stream(metadata).forEach(sum::addAndGet));
+        System.out.println("Part 1 result: " + sum.get());
     }
 
-    private static class Tree {
-        final Tree[] children;
-        final int[] metadata;
-
-        private Tree(Tree[] children, int[] metadata) {
-            this.children = children;
-            this.metadata = metadata;
+    private static int[] consume(int[] input, Consumer<int[]> metadata) {
+        int children = input[0];
+        int metadataLength = input[1];
+        input = Arrays.copyOfRange(input, 2, input.length);
+        for (int child = 0; child < children; child++) {
+            input = consume(input, metadata);
         }
+        metadata.accept(Arrays.copyOfRange(input, 0, metadataLength));
+        return Arrays.copyOfRange(input, metadataLength, input.length);
     }
 }
