f56e660ec06d401241503e135e60f7d04ded15b9
John Wästerlund <grunka@gmail.com>
2018-12-11 23:22:02 +0100

    Slow solution

--- src/main/java/com/grunka/adventofcode/twentyeighteen/Day11.java
+++ src/main/java/com/grunka/adventofcode/twentyeighteen/Day11.java
@@ -1,17 +1,41 @@
 package com.grunka.adventofcode.twentyeighteen;
 
 public class Day11 {
+    private static final int GRID_SERIAL_NUMBER = 9005;
+
     public static void main(String[] args) {
-        if (4 != powerLevel(8, 3, 5)) {
-            throw new Error("Incorrect power level");
+        part1();
+        part2();
+    }
+
+    private static void part2() {
+        int maxX = -1;
+        int maxY = -1;
+        int maxSize = -1;
+        int max = -1;
+        for (int size = 1; size < 300; size++) {
+            for (int y = 1; y <= 300 - size; y++) {
+                for (int x = 1; x <= 300 - size; x++) {
+                    int powerLevel = powerLevelGrid(x, y, size);
+                    if (powerLevel > max) {
+                        max = powerLevel;
+                        maxX = x;
+                        maxY = y;
+                        maxSize = size;
+                    }
+                }
+            }
         }
+        System.out.println("Part 2 result: " + maxX + "," + maxY + "," + maxSize);
+    }
+
+    private static void part1() {
         int maxX = -1;
         int maxY = -1;
         int max = -1;
-        int gridSerialNumber = 9005;
         for (int y = 1; y <= 300 - 3; y++) {
             for (int x = 1; x <= 300 - 3; x++) {
-                int powerLevel = powerLevelGrid(gridSerialNumber, x, y);
+                int powerLevel = powerLevelGrid(x, y, 3);
                 if (powerLevel > max) {
                     max = powerLevel;
                     maxX = x;
@@ -22,22 +46,20 @@ public class Day11 {
         System.out.println("Part 1 result: " + maxX + "," + maxY);
     }
 
-    private static int powerLevelGrid(int gridSerialNumber, int x, int y) {
-        return powerLevel(gridSerialNumber, x, y) +
-                powerLevel(gridSerialNumber, x + 1, y) +
-                powerLevel(gridSerialNumber, x + 2, y) +
-                powerLevel(gridSerialNumber, x, y + 1) +
-                powerLevel(gridSerialNumber, x + 1, y + 1) +
-                powerLevel(gridSerialNumber, x + 2, y + 1) +
-                powerLevel(gridSerialNumber, x, y + 2) +
-                powerLevel(gridSerialNumber, x + 1, y + 2) +
-                powerLevel(gridSerialNumber, x + 2, y + 2);
+    private static int powerLevelGrid(int x, int y, int size) {
+        int sum = 0;
+        for (int a = 0; a < size; a++) {
+            for (int b = 0; b < size; b++) {
+                sum += powerLevel(x + a, y + b);
+            }
+        }
+        return sum;
     }
 
-    private static int powerLevel(int gridSerialNumber, int x, int y) {
+    private static int powerLevel(int x, int y) {
         int rackId = x + 10;
         int powerLevel = rackId * y;
-        powerLevel = powerLevel + gridSerialNumber;
+        powerLevel = powerLevel + Day11.GRID_SERIAL_NUMBER;
         powerLevel = powerLevel * rackId;
         int hundredsDigit = (powerLevel / 100) % 10;
         return hundredsDigit - 5;
