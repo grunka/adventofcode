41cdae67aac7258902f992a2f121889f032ffcb6
John Wästerlund <grunka@gmail.com>
2018-12-11 20:16:01 +0100

    Part 2

--- src/main/java/com/grunka/adventofcode/twentyeighteen/Day08.java
+++ src/main/java/com/grunka/adventofcode/twentyeighteen/Day08.java
@@ -12,18 +12,45 @@ public class Day08 {
     public static void main(String[] args) throws URISyntaxException, IOException {
         int[] input = Arrays.stream(Files.readString(Paths.get(Day08.class.getResource("/twentyeighteen/Day08-1.txt").toURI())).trim().split(" ")).mapToInt(Integer::parseInt).toArray();
         AtomicInteger sum = new AtomicInteger();
-        consume(input, metadata -> Arrays.stream(metadata).forEach(sum::addAndGet));
+        Result result = consume(input, metadata -> Arrays.stream(metadata).forEach(sum::addAndGet));
         System.out.println("Part 1 result: " + sum.get());
+        System.out.println("Part 2 result: " + result.weight);
     }
 
-    private static int[] consume(int[] input, Consumer<int[]> metadata) {
+    private static Result consume(int[] input, Consumer<int[]> metadataConsumer) {
         int children = input[0];
         int metadataLength = input[1];
         input = Arrays.copyOfRange(input, 2, input.length);
+        int[] weights = new int[children];
         for (int child = 0; child < children; child++) {
-            input = consume(input, metadata);
+            Result result = consume(input, metadataConsumer);
+            input = result.rest;
+            weights[child] = result.weight;
         }
-        metadata.accept(Arrays.copyOfRange(input, 0, metadataLength));
-        return Arrays.copyOfRange(input, metadataLength, input.length);
+        int[] metadata = Arrays.copyOfRange(input, 0, metadataLength);
+        int weight = 0;
+        if (children == 0) {
+            for (int index : metadata) {
+                weight += index;
+            }
+        } else {
+            for (int index : metadata) {
+                if (index > 0 && index <= weights.length) {
+                    weight += weights[index - 1];
+                }
+            }
+        }
+        metadataConsumer.accept(metadata);
+        return new Result(weight, Arrays.copyOfRange(input, metadataLength, input.length));
+    }
+
+    private static class Result {
+        final int weight;
+        final int[] rest;
+
+        private Result(int weight, int[] rest) {
+            this.weight = weight;
+            this.rest = rest;
+        }
     }
 }
