a2779cf5c91eb1a98e5269d95efe82508eea8102
John Wästerlund <grunka@gmail.com>
2018-12-13 18:09:05 +0100

    Some progress

--- src/main/java/com/grunka/adventofcode/twentyeighteen/Day13.java
+++ src/main/java/com/grunka/adventofcode/twentyeighteen/Day13.java
@@ -5,7 +5,10 @@ import java.lang.invoke.MethodHandles;
 import java.net.URISyntaxException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Day13 {
     public static void main(String[] args) throws URISyntaxException, IOException {
@@ -14,5 +17,152 @@ public class Day13 {
         int width = lines.stream().mapToInt(String::length).max().orElseThrow();
 
         char[][] track = new char[width][height];
+        for (int y = 0; y < height; y++) {
+            for (int x = 0; x < width; x++) {
+                String row = lines.get(y);
+                if (x < row.length()) {
+                    track[x][y] = row.charAt(x);
+                } else {
+                    track[x][y] = ' ';
+                }
+            }
+        }
+        //print(track);
+
+        List<Cart> carts = getCarts(track, width, height);
+        Collections.shuffle(carts);
+        Collections.sort(carts);
+        System.out.println("carts = " + carts);
+        carts = carts.stream().map(c -> c.move(track)).sorted().collect(Collectors.toList());
+        System.out.println("carts = " + carts);
+    }
+
+    private static List<Cart> getCarts(char[][] track, int width, int height) {
+        List<Cart> carts = new ArrayList<>();
+        for (int y = 0; y < height; y++) {
+            for (int x = 0; x < width; x++) {
+                switch (track[x][y]) {
+                    case '<':
+                        track[x][y] = '-';
+                        carts.add(new Cart(x, y, "left"));
+                        break;
+                    case '>':
+                        track[x][y] = '-';
+                        carts.add(new Cart(x, y, "right"));
+                        break;
+                    case '^':
+                        track[x][y] = '|';
+                        carts.add(new Cart(x, y, "up"));
+                        break;
+                    case 'V':
+                        track[x][y] = '|';
+                        carts.add(new Cart(x, y, "down"));
+                        break;
+                    default:
+                }
+            }
+        }
+        return carts;
+    }
+
+    private static class Cart implements Comparable<Cart> {
+        final int x;
+        final int y;
+        final Direction direction;
+
+        Cart(int x, int y, Direction direction) {
+            this.x = x;
+            this.y = y;
+            this.direction = direction;
+        }
+
+        Cart move(char[][] track) {
+            int nextX = x;
+            int nextY = y;
+            switch (direction) {
+                case UP:
+                    nextY--;
+                    break;
+                case DOWN:
+                    nextY++;
+                    break;
+                case LEFT:
+                    nextX--;
+                    break;
+                case RIGHT:
+                    nextX++;
+                    break;
+            }
+            Direction nextDirection = direction;
+            switch (track[nextX][nextY]) {
+                case '/':
+                    nextDirection = direction == Direction.LEFT || direction == Direction.RIGHT ? direction.left() : direction.right();
+                    break;
+                case '\\':
+                    nextDirection = direction == Direction.LEFT || direction == Direction.RIGHT ? direction.right() : direction.left();
+                    break;
+                case '+':
+                    //TODO
+                    break;
+            }
+            return this;
+        }
+
+        @Override
+        public int compareTo(Cart o) {
+            int row = Integer.compare(y, o.y);
+            return row != 0 ? row : Integer.compare(x, o.x);
+        }
+
+        @Override
+        public String toString() {
+            return "Cart{" +
+                    "x=" + x +
+                    ", y=" + y +
+                    ", direction='" + direction + '\'' +
+                    '}';
+        }
+    }
+
+    @SuppressWarnings("Duplicates")
+    private enum Direction {
+        UP, DOWN, LEFT, RIGHT;
+
+        Direction left() {
+            switch (this) {
+                case UP:
+                    return LEFT;
+                case LEFT:
+                    return DOWN;
+                case DOWN:
+                    return RIGHT;
+                case RIGHT:
+                    return UP;
+            }
+            throw new IllegalStateException();
+        }
+
+        Direction right() {
+            switch (this) {
+                case UP:
+                    return RIGHT;
+                case RIGHT:
+                    return DOWN;
+                case DOWN:
+                    return LEFT;
+                case LEFT:
+                    return UP;
+            }
+            throw new IllegalStateException();
+        }
+    }
+
+    private static void print(char[][] track) {
+        for (int y = 0; y < track[0].length; y++) {
+            for (int x = 0; x < track.length; x++) {
+                System.out.print(track[x][y]);
+            }
+            System.out.println();
+        }
     }
 }
