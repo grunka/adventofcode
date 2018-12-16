2c400258352ab5e8d0058e749a774d2a7e9907f8
John Wästerlund <grunka@gmail.com>
2018-12-16 12:13:49 +0100

    Switching from list to array

--- src/main/java/com/grunka/adventofcode/twentyeighteen/Day14.java
+++ src/main/java/com/grunka/adventofcode/twentyeighteen/Day14.java
@@ -1,40 +1,43 @@
 package com.grunka.adventofcode.twentyeighteen;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import java.util.stream.Collectors;
 
 public class Day14 {
     public static void main(String[] args) {
-        List<Integer> recipes = new ArrayList<>(List.of(3, 7));
+        int[] recipes = new int[]{3, 7};
         int elf1 = 0;
         int elf2 = 1;
         //print(recipes, elf1, elf2);
         boolean targetFound = false;
         boolean otherTargetFound = false;
         int target = 846601;
-        List<Integer> otherTarget = Arrays.stream(String.valueOf(target).split("")).map(Integer::valueOf).collect(Collectors.toList());
+        int[] otherTarget = Arrays.stream(String.valueOf(target).split("")).mapToInt(Integer::valueOf).toArray();
         while (!targetFound || !otherTargetFound) {
-            int newRecipe = recipes.get(elf1) + recipes.get(elf2);
+            int newRecipe = recipes[elf1] + recipes[elf2];
             if (newRecipe >= 10) {
-                recipes.add(newRecipe / 10);
-                recipes.add(newRecipe % 10);
+                recipes = Arrays.copyOf(recipes, recipes.length + 2);
+                recipes[recipes.length - 2] = newRecipe / 10;
+                recipes[recipes.length - 1] = newRecipe % 10;
             } else {
-                recipes.add(newRecipe);
+                recipes = Arrays.copyOf(recipes, recipes.length + 1);
+                recipes[recipes.length - 1] = newRecipe;
             }
-            elf1 = (elf1 + recipes.get(elf1) + 1) % recipes.size();
-            elf2 = (elf2 + recipes.get(elf2) + 1) % recipes.size();
+            elf1 = (elf1 + recipes[elf1] + 1) % recipes.length;
+            elf2 = (elf2 + recipes[elf2] + 1) % recipes.length;
             //print(recipes, elf1, elf2);
-            boolean targetIsFound = targetFound || recipes.size() >= target + 10;
+            boolean targetIsFound = targetFound || recipes.length >= target + 10;
             if (!targetFound && targetIsFound) {
-                String result = recipes.subList(target, target + 10).stream().map(String::valueOf).collect(Collectors.joining());
-                System.out.println("Part 1 result: " + result);
+                System.out.print("Part 1 result: ");
+                for (int i = 0; i < 10; i++) {
+                    System.out.print(recipes[target + i]);
+                }
+                System.out.println();
             }
             targetFound = targetFound || targetIsFound;
-            boolean otherTargetIsFound = otherTargetFound || recipes.size() > otherTarget.size() && recipes.subList(recipes.size() - otherTarget.size(), recipes.size()).equals(otherTarget);
+            boolean otherTargetIsFound = otherTargetFound || recipes.length > otherTarget.length && Arrays.equals(recipes, recipes.length - otherTarget.length, recipes.length, otherTarget, 0, otherTarget.length);
             if (!otherTargetFound && otherTargetIsFound) {
-                System.out.println("Part 2 result: " + (recipes.size() - otherTarget.size()));
+                System.out.println("Part 2 result: " + (recipes.length - otherTarget.length));
             }
             otherTargetFound = otherTargetFound || otherTargetIsFound;
         }
