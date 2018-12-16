d6ad68e8561de02f4df60891f6c15e7f75d1715a
John Wästerlund <grunka@gmail.com>
2018-12-16 12:21:16 +0100

    Trying to speed it up but still eating too much memory

--- src/main/java/com/grunka/adventofcode/twentyeighteen/Day14.java
+++ src/main/java/com/grunka/adventofcode/twentyeighteen/Day14.java
@@ -1,11 +1,11 @@
 package com.grunka.adventofcode.twentyeighteen;
 
 import java.util.Arrays;
-import java.util.List;
 
 public class Day14 {
     public static void main(String[] args) {
         int[] recipes = new int[]{3, 7};
+        int recipes_length = 2;
         int elf1 = 0;
         int elf2 = 1;
         //print(recipes, elf1, elf2);
@@ -16,17 +16,23 @@ public class Day14 {
         while (!targetFound || !otherTargetFound) {
             int newRecipe = recipes[elf1] + recipes[elf2];
             if (newRecipe >= 10) {
-                recipes = Arrays.copyOf(recipes, recipes.length + 2);
-                recipes[recipes.length - 2] = newRecipe / 10;
-                recipes[recipes.length - 1] = newRecipe % 10;
+                recipes_length += 2;
+                if (recipes_length > recipes.length) {
+                    recipes = Arrays.copyOf(recipes, recipes.length * 2);
+                }
+                recipes[recipes_length - 2] = newRecipe / 10;
+                recipes[recipes_length - 1] = newRecipe % 10;
             } else {
-                recipes = Arrays.copyOf(recipes, recipes.length + 1);
-                recipes[recipes.length - 1] = newRecipe;
+                recipes_length++;
+                if (recipes_length > recipes.length) {
+                    recipes = Arrays.copyOf(recipes, recipes.length * 2);
+                }
+                recipes[recipes_length - 1] = newRecipe;
             }
-            elf1 = (elf1 + recipes[elf1] + 1) % recipes.length;
-            elf2 = (elf2 + recipes[elf2] + 1) % recipes.length;
-            //print(recipes, elf1, elf2);
-            boolean targetIsFound = targetFound || recipes.length >= target + 10;
+            elf1 = (elf1 + recipes[elf1] + 1) % recipes_length;
+            elf2 = (elf2 + recipes[elf2] + 1) % recipes_length;
+            //print(recipes, recipes_length, elf1, elf2);
+            boolean targetIsFound = targetFound || recipes_length >= target + 10;
             if (!targetFound && targetIsFound) {
                 System.out.print("Part 1 result: ");
                 for (int i = 0; i < 10; i++) {
@@ -35,9 +41,9 @@ public class Day14 {
                 System.out.println();
             }
             targetFound = targetFound || targetIsFound;
-            boolean otherTargetIsFound = otherTargetFound || recipes.length > otherTarget.length && Arrays.equals(recipes, recipes.length - otherTarget.length, recipes.length, otherTarget, 0, otherTarget.length);
+            boolean otherTargetIsFound = otherTargetFound || recipes_length > otherTarget.length && Arrays.equals(recipes, recipes_length - otherTarget.length, recipes_length, otherTarget, 0, otherTarget.length);
             if (!otherTargetFound && otherTargetIsFound) {
-                System.out.println("Part 2 result: " + (recipes.length - otherTarget.length));
+                System.out.println("Part 2 result: " + (recipes_length - otherTarget.length));
             }
             otherTargetFound = otherTargetFound || otherTargetIsFound;
         }
@@ -45,9 +51,9 @@ public class Day14 {
         //System.out.println("recipes.size() = " + recipes.size());
     }
 
-    static void print(List<Integer> recipes, int elf1, int elf2) {
-        for (int i = 0; i < recipes.size(); i++) {
-            int value = recipes.get(i);
+    static void print(int[] recipes, int recipes_length, int elf1, int elf2) {
+        for (int i = 0; i < recipes_length; i++) {
+            int value = recipes[i];
             if (i == elf1) {
                 System.out.print("(" + value + ")");
             } else if (i == elf2) {
